8e24c1ad982582a0a57e38657ba15246c7b73d16
SkyGuy <62302720+mr-SkyDev@users.noreply.github.com>
2021-10-03 17:29:05 +0300

    Add files via upload

---
+++ 04.10HW/fromIntToStr.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+const string strNums[11] = {
+      "0", "1", "2", "3", "4", "5",
+      "6", "7", "8", "9", "10"
+  };
+
+
+string to_str(int n) {
+    string result = "";
+
+    int x = n;  // Чтобы не изменять исходное число
+    do {
+        result += strNums[abs(x % 10)];
+        x /= 10;
+    } while (x);
+
+    result = n >= 0 ? result : result + "-"; 
+    reverse(result.begin(), result.end());
+
+    return result;
+}
+
+
+int main() {
+  int number;
+  cin >> number;
+  cout << to_str(number);
+
+  return 0;
+}
