0351be4105577346542840d018b996d72962feea
SkyGuy <62302720+mr-SkyDev@users.noreply.github.com>
2021-10-03 17:34:11 +0300

    Update fromIntToStr.cpp

--- 04.10HW/fromIntToStr.cpp
+++ 04.10HW/fromIntToStr.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 
-const string strNums[11] = {
-      "0", "1", "2", "3", "4", "5",
-      "6", "7", "8", "9", "10"
+const string strNums[10] = {
+      "0", "1", "2", "3", "4", 
+      "5", "6", "7", "8", "9"
   };
 
 
