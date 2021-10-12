127289133f54e1c926c8295806b338fc7053646b
Площадь и периметр треугольника по координатам <nurmagomedoffzaur@yandex.ru>
2021-10-12 14:23:57 +0300

    Массив симвлов. Функции

--- 12.10CW/main.cpp
+++ 12.10CW/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
-    // Создать "строку" мы можем с помощью массива символов
+    string name1 = "Z@yЯ";  // Создание строки
+
     char name[20] = "";
-    cout << "Введите имя" << "\n";
-    cin.getline(name, 20);  // Получение ввода по строкам (через Enter), \0 добавляется автоматически
-    cout << "Hello, " << name << "!" << endl;
+    char s1[20] = "";
+    cout << "Hi, " << name << endl;
+    cout << "Длина имени: " << strlen(name);  // Работает только для массива символов (char)
+    strcpy(s1, name);  // s1 = name[:]. Также работает только для char
+    strncpy(s1, name, 4);  // s1 = name[:4]. Также работает только для char
+    strcat(s1, name);  // s1 = s1 + name. Также работает только для char
+
+    char *n = strchr(name, 'a');  // Индекс на 'a'. Аналог string.index() в python
+    char c;
+    isalnum(c);  // проверка на букву либо цифру
+    isalpha(c);  // Проверка на букву
+    isdigit(c);  // Проверка на цифру
+    islower(c);  // Проверка на нижний регистр
+    isupper(c);  // Проверка на верхний регистр
+    isspace(c);  // Проверка на пробельный символ
+    toupper(c);  // В верхний регистр
+    tolower(c);  // В нижний регистр
+    atof(s1);  // преобразование в double
+    atoi(s1);  // преобразование в int
+    atol(s1);  // преобразование в long int
+
+    // Все эти ф-ии только для char
 }
