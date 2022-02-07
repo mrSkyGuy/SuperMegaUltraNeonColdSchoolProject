5e2e0a3b4085cac77800a2672825212e1441760a
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-02-07 23:21:27 +0300

    release pretty print func

---
+++ FirstCSharpProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Project {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Введите сообщение: ");
+            PrettyPrint(Console.ReadLine());
+        }
+
+        static void PrettyPrint(string? msg) {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+    }
+}
+
