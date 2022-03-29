c10018b8d5346e8fd99ad19f59bdd61850ef3059
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-03-29 21:32:34 +0300

    add bar and collide with ball

--- Csharp/ArkanoidGame/Program.cs
+++ Csharp/ArkanoidGame/Program.cs
@@ -4,8 +4,8 @@ using System.Diagnostics;
 
 // cd C:\Users\nurma\myLife-xD\SuperMegaUltraNeonColdSchoolProject\Csharp\ArkanoidGame\ && dotnet run
 struct ConsoleSettings {
-    static public int height = 40,
-                      width = 110, 
+    static public int height = 35,
+                      width = 70, 
                       fps = 10;
 }
 
@@ -18,8 +18,19 @@ class Program {
         
         bool run = true;
         Ball ball = new Ball();
+        Bar bar = new Bar();
         while (run) {
+            if (Console.KeyAvailable) switch (Console.ReadKey().Key) {
+                case ConsoleKey.LeftArrow:
+                    bar.moveLeft();
+                    break;
+                case ConsoleKey.RightArrow:
+                    bar.moveRight();
+                    break;
+            }
             ball.move();
+            ball.checkColideWithBar(bar);
+            bar.show();
             System.Threading.Thread.Sleep(1000 / ConsoleSettings.fps);
             if (!ball.isAlive) run = false;
         }
@@ -56,8 +67,8 @@ class Ball {
         show();
 
         if ((coordX >= ConsoleSettings.width - 1) || (coordX <= 2)) speedX *= -1;
-        if (coordY >= ConsoleSettings.height) { remove(); hide(); }
-        if (coordY <= 2) speedY *= -1;
+        if (coordY >= ConsoleSettings.height - 1) { remove(); hide(); }
+        if (coordY <= 0) speedY *= -1;
     }
  
     public void show() {
@@ -76,4 +87,59 @@ class Ball {
         isAlive = false;
         symb = ' ';
     }
+
+    public void checkColideWithBar(Bar bar) {
+        if (bar.coordsX.Contains(this.coordX) && bar.coordY == this.coordY) speedY *= -1;
+    }
+}
+
+
+class Bar {
+    public int width { get; set; } 
+    public int coordX { get; set; } 
+    public int[] coordsX { get; set; }
+    public int coordY { get; set; } 
+    public int speedX { get; set; } 
+
+    private char symb = '=';
+
+    public Bar() {
+        width = 10;
+        coordX = ConsoleSettings.width / 2 - width / 2;
+        coordY = ConsoleSettings.height - 2;
+        speedX = 2;
+
+        updateCoordsX();
+    }
+
+    public void show() {
+        Console.SetCursorPosition(coordX, coordY);
+        Console.Write(":");
+        for (int i = 0; i < width - 2; i++) Console.Write(symb);
+        Console.Write(":");
+    }
+
+    public void hide() {
+        Console.SetCursorPosition(coordX, coordY);
+        for (int i = 0; i < width; i++) Console.Write(' ');
+    }
+
+    public void moveLeft() {
+        hide();
+        coordX -= speedX;
+        if (coordX <= 1) coordX = 2; 
+        updateCoordsX();
+    }
+
+    public void moveRight() {
+        hide();
+        coordX += speedX;
+        if (coordX >= ConsoleSettings.width - width - 1) coordX = ConsoleSettings.width - width - 2;
+        updateCoordsX();
+    }
+
+    private void updateCoordsX() {
+        coordsX = new int[width];
+        for (int i = 0; i < width; i++) coordsX[i] = coordX + i;
+    } 
 }
